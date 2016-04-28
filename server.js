b728a912c7fbdb15fa4da6ea7c806459996e8a26
Caleb Whiting <whmountains@icloud.com>
2016-04-28 19:28:06 -0300

    reorganize and simplify

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var connect = require('connect')
 var morgan = require('morgan')
 var serveStatic = require('serve-static')
-var path = require('path')
 
 var app = connect()
 
 app.use(morgan('dev'))
-app.use(serveStatic(path.resolve('./dist')))
+
+app.use(serveStatic(__dirname))
 
 var listenPort = 3000
 app.listen(listenPort)
